8f151b50b21e6cdc25722702d8b3a3df5f2dbb46
Jonas <jonas_barros@outlook.com>
2019-09-14 15:49:42 -0300

    Listando e salvando os produtos do ecomemrce

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from api.ProductsviewSet import ProductsViewSet
+from products.api.ProductsviewSet import ProductsViewSet
 
 urlpatterns = [
-    path('products', ProductsViewSet.as_view({'get': 'listProducts'}), name='listProducts')
-    path('products/<id:int>', ProductsViewSet.as_view({'get': 'findOne'}), name=True)
-
+    path('products', ProductsViewSet.as_view({'get': 'allProducts', 'post': 'create'}), name='products'),
+    
+    path('products/<int:id>', ProductsViewSet.as_view({'get': 'findOneProduct'}), name='findOneProducts')
 ]
