c5742215d17f398e25e4b7620f5778bde1578950
Jonas <jonas_barros@outlook.com>
2019-09-21 21:11:15 -0300

    Desenvolvendo o método que centraliza todas as consultas no banco de dados, e já implementamos todos os metodos dentro da viewSet Cftv

---
+++ djangoQuerySet/djangoQuerySet.py
@@ -0,0 +1,87 @@
+from .querysetValidate import djangoQuerysetValidate
+from rest_framework.response import Response
+import json
+
+"""
+Todos os modulos da aplicação que consite em salvar, 
+editar, deletar e listar, tera qiefazer uso dessa classe.. 
+
+Pra que ela fiqiue totalmente desviculada da aplicação como um tood
+é porobido haver algum import de algum modulo dentro dela
+
+Author: Jonas Florencio 
+Data: 21/09/2019
+Ultima atualização: 21/09/2019
+"""
+
+class djangoQuerySet:
+    def __init__(self):
+        self.__djangoQuerysetValidate = djangoQuerysetValidate()
+
+
+    def listFull(self, model, classSerializer, many = None, read_only = False):
+        """
+        model: Nome da modelagem da aplicação
+        classSerializer: Nome do serializador que voce está usado
+        many: é aceitavel multiplos vaores
+        read_only: Num sei o que é ainda
+        """
+
+        self.__instance = hasattr(model, '__name__')
+
+        if(self.__instance == False):
+            self.__djangoQuerysetValidate.checkInstance('model', self.__instance)
+
+        else:
+            queryset = model.objects.all()
+            serializer = classSerializer(queryset, many = many, read_only = read_only)
+
+            return serializer.data
+
+    def filterOne(self, ids, model, classSerializer, many=True, read_only = False):
+        
+        self.__instance = hasattr(model, '__name__')
+
+        if(self.__instance == False): 
+            self.__djangoQuerysetValidate.checkInstance('model', self.__instance)
+
+        else:    
+            queryset = model.objects.filter(id = ids)
+            serializer = classSerializer(queryset, many = many, read_only = read_only)
+
+            return serializer.data
+    
+    def filterPrice(self, price, model, classSerializer,
+                                   many = True, read_only = False):
+        
+        price = price.split("ate")
+        formatePrice = json.dumps(price)
+        listPrice = json.loads(formatePrice)
+
+        lowerPrice = listPrice[0]
+        higherPrice = listPrice[1]
+
+        queryset = model.objects.filter(products__price__gte = lowerPrice
+        ).filter(products__price__lte = higherPrice)
+        
+        serializer = classSerializer(queryset, many = many, read_only = read_only)
+
+        return serializer.data
+    
+    def filterMake(self, make, model, classSerializer, many = True, read_only = False):
+
+        self.__instance = hasattr(model, "__name__")
+
+        if(self.__instance == False):
+            self.__djangoQuerysetValidate.checkInstance('model', self.__instance)
+        
+        else:
+            queryset = model.objects.filter(products__make = make)
+            serializer = classSerializer(queryset, many = many, read_only = read_only)
+
+            return serializer.data
+        
+        
+
+
+
--- products/api/Categories/Cftv/CftvViewSet.py
+++ products/api/Categories/Cftv/CftvViewSet.py
@@ -5,11 +5,16 @@ from rest_framework import status
 from products.models import Cftv
 from .CftvSerializer import CftvSerializer
 
+from djangoQuerySet.djangoQuerySet import djangoQuerySet
 import json
 
-class CftvViewSet(ModelViewSet):
+class CftvViewSet(ModelViewSet, djangoQuerySet):
 
-    serializer_class = CftvSerializer                                                                             
+    serializer_class = CftvSerializer             
+    model = Cftv
+
+    def __init__(self):
+        self.__djangoQuerySet = djangoQuerySet() 
 
     def create(self, request,  *args, **kwargs):
         
@@ -21,36 +26,20 @@ class CftvViewSet(ModelViewSet):
     
     def list(self, request, *args, **kwargs):
 
-        cftv = Cftv.objects.all()
-        cftvSerializer = CftvSerializer(cftv, many = True)
-        print(f">>>> {cftvSerializer.data}")
+        cftvSerializer = self.__djangoQuerySet.listFull(self.model, CftvSerializer)
+        return Response(cftvSerializer, status = status.HTTP_200_OK)
 
-        return Response(cftvSerializer.data, status = status.HTTP_200_OK)
+    def findOne(self, request, ids):
 
-    def findOne(self, request, id):
-
-        cftv = Cftv.objects.filter(id = id)
-        cftvSerializer = CftvSerializer(cftv, many=True)
-
-        return Response(cftvSerializer.data, status = status.HTTP_200_OK)
+        cftvSerializer  = self.__djangoQuerySet.filterOne(ids, self.model, CftvSerializer)
+        return Response(cftvSerializer, status = status.HTTP_200_OK)
 
     def findPrice(self, request, price):
 
-        price = price.split("ate")
-        formatprice = json.dumps(price)
-        listPrice = json.loads(formatprice)
-        lowerPrice = listPrice[0]
-        higherPrice = listPrice[1]
-        
-
-        cftv = Cftv.objects.filter(products__price__gte = lowerPrice).filter(products__price__lte = higherPrice)
-        cftvSerializer = CftvSerializer(cftv, many=True)
-
-        return Response(cftvSerializer.data, status = status.HTTP_200_OK)
+        cftvSerializer = self.__djangoQuerySet.filterPrice(price, self.model, CftvSerializer)
+        return Response(cftvSerializer, status = status.HTTP_200_OK)
     
     def findMake(self, request, make):
-    
-        cftv = Cftv.objects.filter(products__make = make)
-        cftvSerializer = CftvSerializer(cftv, many=True)
 
-        return Response(cftvSerializer.data, status = status.HTTP_200_OK)
+        cftvSerializer = self.__djangoQuerySet.filterMake(make, self.model, CftvSerializer)
+        return Response(cftvSerializer, status = status.HTTP_200_OK)
