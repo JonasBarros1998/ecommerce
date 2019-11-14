e859154c91fd95b38935017901ac3659207134e4
Jonas <jonas_barros@outlook.com>
2019-11-13 21:19:47 -0300

    Alterando o endereço das rotas para a geração do token, alterando os campos de dados cadastrais que o usuario vai precisar registrar caso queira fazer uma compra

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,1 @@
-from django.urls import path
-from authentication.api.oauth2.gearToken import GearToken
 
-urlpatterns = [
-    path("authorizeToken/", GearToken.as_view({'post':'token'}), name="token"),
-]
