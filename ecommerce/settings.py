e587b3e9650d192545b5dcd6b9b09622fbdd619c
Jonas Florencio de Barros <jonas_barros@outlook.com>
2019-12-13 21:19:25 -0300

    Desenvolvendo as funcionalidades responsaveis por criar novos usuarios, e adicionando-os dentro de table ade login. Com isso são duas tabelas, auth_user: authentication e users_registeruser: para informações gerais do usuario.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'authentication',
     'products',
     'shop',
-    'Users'
+    'users'
 ]
 
 MIDDLEWARE = [
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('jm-ecommerce/', include('products.urls')), 
     path('jm-ecommerce/', include('comments.urls')),
-    path('jm-ecommerce/', include('Users.urls')),
+    path('jm-ecommerce/', include('users.urls')),
     path('jm-ecommerce/authentication/', include('oauth2_provider.urls'))
 ]
