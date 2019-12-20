d4e6087749c87855ae34c4aa709b1db6d86e4f72
Jonas Florencio de Barros <jonas_barros@outlook.com>
2019-12-20 08:29:02 -0300

    Refatorando o modulo de cftv. Foram adicionado comentarios dentro das urls, e das funções do djangoQuerySelector

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from products.api.ProductsviewSet import ProductsViewSet
-from products.api.Categories.Cftv.CftvViewSet import CftvViewSet
+from products.categories.categorieCftv.viewsets.CftvViewSet import CftvViewSet
 
 from products.api.Categories.AirPhones.airPhonesViewSet import AirPhonesViewSet
 from products.api.Categories.SmartWatch.smartWathViewSet import SmartWathViewSet
@@ -14,16 +14,18 @@ urlpatterns = [
     path('products/categories', ProductsViewSet.as_view({'get':'findCategoriesAll'}), name='findCategories'),
     path('products/categories/<str:categories>', ProductsViewSet.as_view({'get':'findOneCategories'}), name="findOneCategories"),
 
-    #Rotas para produtos de CFTV
+    #Rotas para produtos de cftv
     path('products/cftv', CftvViewSet.as_view({'get': 'list', 'post': 'create'}), name='products'),
     path('products/cftv/<int:ids>', CftvViewSet.as_view({'get': 'findOne'}), name='findOne'),
     path('products/ctfv/price/<str:price>', CftvViewSet.as_view({'get': 'findPrice'}), name='findPrice'),
     path('products/ctfv/make/<str:make>', CftvViewSet.as_view({'get': 'findMake'}), name='findMake'),
     
+    #Rotas para produtos de airphone
     path('products/airPhone', AirPhonesViewSet.as_view({'get':'list', 'post':'create'}), name='airPhone'),
     path('products/airPhone/<int:air_phone_ids>', AirPhonesViewSet.as_view({'get':'findOne'}), name='findOneAirPhones'),
     path('products/airPhone/<str:air_phone_make>', AirPhonesViewSet.as_view({'get':'findMakeAll'}), name="airPhoneMake"),
 
+    #Rotas para produtos de smartwatch
     path('products/smartWatch', SmartWathViewSet.as_view({'post':'create', 'get':'list'}), name='smartWatch'),
     path('products/smartWatch/<int:smart_watch_ids>', SmartWathViewSet.as_view({'get':'findOne'}), name='findOneSmartWatch'),
     path('products/smartWatch/<str:smart_watch_make>', SmartWathViewSet.as_view({'get':'findMakeAll'}), name='findMakeAll')
